47a2701038eb8814742e2c894fa7798512a896e3
DomBurke23 <dominiqueburke@hotmail.co.uk>
2021-04-22 16:50:40 +0100

    Add files via upload

---
+++ Forest.cs
@@ -0,0 +1,114 @@
+using System;
+
+namespace BasicClasses
+{
+  class Forest // defining a class 
+  {
+    // defining fields (class members)
+    // common practice is to use all lowercase for fields 
+    public string name; 
+    /* Go back to ForestMain and change the field from f.name to f.Name */
+    public string Name // property
+    {
+      get { return name; }
+      set { name = value; }
+    }
+
+    //public int trees; 
+    public int Trees
+    {
+        /*
+        * get { return trees; }
+        * set { trees = value; } 
+        * The following is the same as above but written as an 
+        * automatic property. 
+        * must remove field trees */
+        get; set; 
+    }
+
+    public int age; 
+    public int Age
+    { 
+      get { return age; }
+      private set { age = value; }
+    }
+    
+    private string biome; 
+    public string Biome
+    {
+        get{return biome;}
+        set{
+          if(value == "Tropical" || value == "Temperate" || value == "Boreal"){
+              Console.WriteLine(value); biome = value; }
+          else{
+              Console.WriteLine("Unknown"); biome="Unknown"; }
+        }
+    }
+    
+    /* Properties - validation for values */
+    public int area;
+    public int Area // Area property is associated with the area field. 
+    {
+        get { return area; } // called when the property is accessed
+        /* The set() method above uses the keyword value, which 
+         * represents the value we assign to the property. */
+        //set { area = value; } // no validation here 
+        set { //  called when the property is assigned a value
+            if (value < 0) { area = 0; }
+            else { area = value; }
+        }
+    }
+    
+    /* By applying public and private, we can also use properties to control access to fields. 
+     * The following allows us to get the value but not set the value of circumference. */
+    public int circumference;
+    public int Circumference
+    {
+        get {return circumference;}
+        private set {circumference = value;}
+    }
+    
+    /* The third type of member in classes is methods. */
+    public int IncreaseArea(int growth)
+    {
+        Area = Area + growth;
+        return Area;
+    }
+    
+    public int Grow(){
+      Trees =+ 30;
+      Age =+ 1;
+      return Trees;
+    }
+    public int Burn(){
+      Trees =- 20;
+      Age =+ 1; 
+      return Trees; 
+    }
+    
+    /* Constructors - look like a method but have no return type and name is the same as the class name */
+    public int Radius; 
+    public Forest(int radius,string name, string biome)
+    {
+        /* to make the following instance of the class more explicit using the word this.
+         * means “when this constructor is used to make a new instance, use the argument 
+         * area to set the value of this new instance’s Area field” */
+        this.Radius = radius; // set value to fields 
+        this.Name = name;
+        this.Biome = biome; 
+        this.Age = 0; 
+    }
+    
+    /* Overloading Constructors */
+    /*public Forest(int radius)
+    {
+        this.Radius = area; 
+        this.Name = "Unknown"; 
+    }*/
+    /* Reduce duplicate code when using Overloading Constructors */
+    public Forest(int radius):this(radius,"Unknown2","Unknown3")
+    {
+        Console.WriteLine("Country property not specified. Value defaulted to 'Unknown'.");
+    }
+  }
+}
---
+++ ForestMain.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace BasicClasses
+{
+  class ForestMain
+  {
+    static void Main(string[] args)
+    {
+        /* phrase is an instance of the string type. 
+         * Every string has a Length property and IndexOf() method, 
+         * but the resulting values are different for each instance. */
+        string phrase = "zoinks!";
+        Console.WriteLine(phrase.Length);
+        Console.WriteLine(phrase.IndexOf("k"));
+        
+        /* A class represents a custom data type. 
+         * In C#, the class defines the kinds of information 
+         * and methods included in a custom type.
+         * You can then make instances of that class 
+         * (above, phrase was an instance of string). 
+         * There may be many instances of the same class, all with unique values.*/
+         
+        /* The code for class Forest is in its own .cs file for debugging 
+         * create an instance/object of that class with keyword new 
+         * The process of creating an instance is called instantiation. */
+        //Forest f = new Forest(); // parameterless constructor
+        Forest f = new Forest(400,"Name1","Biome1"); // this is a constructor. 
+        /* access and edit each field with dot notation */
+        //f.name="Amazon"; // no propety 
+        f.Name="Amazon"; // using properties
+        //f.trees=300; 
+        f.Trees=300; // access using property
+        f.age=130;  // access using field which avoids properties validation by directly accessing the field
+        //f.Age=120; // error due to having private setter 
+        //f.biome="Tropical";
+        f.Biome="Tropical";
+        f.Biome = "Desert"; // the following will cause a crash due to value being invalid 
+        Console.WriteLine($"{f.Name} , {f.Trees} , {f.age} , {f.Biome}"); 
+        Forest f2 = new Forest(600,"Congo","Biome2");
+        f2.name = "Congo";
+        Console.WriteLine("f2.name = " + f2.name);
+        
+        f.Area = -1; // set() is called
+        Console.WriteLine("Area = " + f.Area);
+        int result = f.IncreaseArea(2);
+        Console.WriteLine("result = " + result);
+        Console.WriteLine("Grow() = " + f.Grow());
+        Console.WriteLine("Burn() = " + f.Burn());
+        Forest f3 = new Forest(1000);
+    }
+  }
+}
