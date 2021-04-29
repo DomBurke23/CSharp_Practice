5d4ce64ae5186234a3fa056615620e39f09087b7
DomBurke23 <dominiqueburke@hotmail.co.uk>
2021-04-29 12:48:59 +0100

    Update Diary.cs

--- Diary.cs
+++ Diary.cs
@@ -7,22 +7,27 @@ namespace LearnReferences
 
     public Diary(int page = 0) : base()
     {
-      CurrentPage = page;
+		CurrentPage = page;
     }
 
     public void Flip()
     {
-      CurrentPage++;
+		CurrentPage++;
     }
 
     public string SpillSecret()
     {
-      return "OMG kerry loves kris <3";
+		return "OMG kerry loves kris <3";
     }
 
     public override string Stringify() 
     {
-      return "This is a Diary object!";
+		return "This is a Diary object!";
     }
+	
+	public override string ToString()
+	{
+		return $"This Diary is currently on page {CurrentPage}."; 
+	}
   }
 }
